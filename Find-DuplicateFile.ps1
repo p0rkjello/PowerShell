06d2403adb6d9927906dfd69910b207ef2730f0a
Andrew Bounds <asbounds@gmail.com>
2020-06-18 14:11:08 -0400

    new file

---
+++ Find-DuplicateFile.ps1
@@ -0,0 +1,18 @@
+﻿function Find-DuplicateFile {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [string[]]
+        $Path,
+
+        [ValidateSet('SHA1', 'SHA256', 'SHA384', 'SHA512', 'MD5')]
+        [string]
+        $Algorithm = 'MD5'
+    )
+    process {
+        $Path | Get-ChildItem -File -Recurse -ErrorAction SilentlyContinue |
+            Get-FileHash -Algorithm $Algorithm -ErrorAction SilentlyContinue|
+                Group-Object -Property Hash |
+                    Where-Object { $_.Count -gt 1 }
+    }
+}
